3e09887147330191e6eb052daa7b9788e49290bb
root <root@OSA.osa.wtw.org>
2016-12-24 16:49:49 +0800

    improve
    Signed-off-by: root <root@OSA.osa.wtw.org>

--- prepare.sh
+++ prepare.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 set -ex
 
-apt-get update
-
-apt-get install bridge-utils debootstrap \
-	ifenslave  lsof lvm2 ntp ntpdate \
-	tcpdump vlan aptitude build-essential \
-	git ntp ntpdate python-dev git figlet -y
 export CHANGE_SOURCE=${CHANGE_SOURCE:-"fault"}
 export WORK_PATH=$(pwd)
 export IMAGE_PATH=$WORK_PATH/image
@@ -14,16 +8,28 @@ export TEMPLATEI_PATH=$WORK_PATH/template
 export LAUNCH_VM_PATH=$WORK_PATH/launch-vm
 export IMAGE_URL=$IMAGE_PATH/prod-xen.img
 
-wget https://cloud-images.ubuntu.com/xenial/current/current/xenial-server-cloudimg-amd64-disk1.img $IMAGE_PATH
-mv $IMAGE_PATH/xenial-server-cloudimg-amd64-disk1.img $IMAGE_URL
-
-qemu-img resize $IMAGE_URL +100GB
-
-if ["$CHANGE_SOURCE"x = "true"]
+if [ "$CHANGE_SOURCE"x == "true"x ]
 then
-  cp $TEMPLATEI_PATH/source-list.temp /etc/apt/source-list
-fi 
+  cp $TEMPLATEI_PATH/source-list.temp /etc/apt/sources.list
+  apt-get -s clean
+fi
 
+apt-get update
+
+apt-get install bridge-utils debootstrap \
+        ifenslave  lsof lvm2  ntpdate libvirt-bin \
+        tcpdump vlan aptitude build-essential \
+        ntp ntpdate python-dev git figlet -y
+
+if [ ! -d "$IMAGE_PATH" ]; then 
+  mkdir "$IMAGE_PATH" 
+fi
+
+if [ ! -f "$IMAGE_URL" ]; then 
+  wget https://cloud-images.ubuntu.com/xenial/current/xenial-server-cloudimg-amd64-disk1.img 
+  mv ./xenial-server-cloudimg-amd64-disk1.img $IMAGE_URL
+  qemu-img resize $IMAGE_URL +100GB
+fi
 
 virsh net-create $WORK_PATH/prod.xml
 cp $TEMPLATEI_PATH/interfaces.temp /etc/network/interfaces
