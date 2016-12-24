cccebd94cc1832b9a2a8503a45caf27ae55dd81f
wutianwei <wutianwei1@huawei.com>
2016-12-24 15:49:10 +0800

    modify the source-list if need
    Signed-off-by: wutianwei <wutianwei1@huawei.com>

--- prepare.sh
+++ prepare.sh
@@ -7,20 +7,25 @@ apt-get install bridge-utils debootstrap \
 	ifenslave  lsof lvm2 ntp ntpdate \
 	tcpdump vlan aptitude build-essential \
 	git ntp ntpdate python-dev git figlet -y
-
+export CHANGE_SOURCE=${CHANGE_SOURCE:-"fault"}
 export WORK_PATH=$(pwd)
 export IMAGE_PATH=$WORK_PATH/image
 export TEMPLATEI_PATH=$WORK_PATH/template
-export LAUNCH_VM_PATH=$WORK_PATH/launch_vm
+export LAUNCH_VM_PATH=$WORK_PATH/launch-vm
 export IMAGE_URL=$IMAGE_PATH/prod-xen.img
 
-git clone https://github.com/wtwde/launch_vm.git
 wget https://cloud-images.ubuntu.com/xenial/current/current/xenial-server-cloudimg-amd64-disk1.img $IMAGE_PATH
 mv $IMAGE_PATH/xenial-server-cloudimg-amd64-disk1.img $IMAGE_URL
+
 qemu-img resize $IMAGE_URL +100GB
+
+if ["$CHANGE_SOURCE"x = "true"]
+then
+  cp $TEMPLATEI_PATH/source-list.temp /etc/apt/source-list
+fi 
+
 
 virsh net-create $WORK_PATH/prod.xml
 cp $TEMPLATEI_PATH/interfaces.temp /etc/network/interfaces
-ifdown  -a && ifup -a 
-source $WORK_PATH/deploy.sh
+#ifdown  -a && ifup -a 
 
