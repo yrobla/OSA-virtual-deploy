2ae12a6be41ad424446c14fcdf6aa7e50ad773c1
Yolanda Robla Mota <yroblamo@redhat.com>
2017-01-10 12:02:23 +0100

    Just use one controller and one compute

--- launch-vm/env_config_host.sh
+++ launch-vm/env_config_host.sh
@@ -10,12 +10,12 @@
 # this is a modified copy of bottlenecks/utils/rubbos_dev_env_setup/env_config.sh
 #assist,deploy | ifra1,infra2,infra3,compute1,compute2
 export OS_FAMILY=${OS_FAMILY:-"Ubuntu16.04"}
-export HOSTNAMES=${HOSTNAMES:- "infra1" "infra2" "infra3" "compute1" "compute2"}
+export HOSTNAMES=${HOSTNAMES:- "infra1" "compute1" }
 export VIRT_NUMBER=${VIRT_NUMBER:-"3"}
 export VIRT_MEM=${VIRT_MEM:-"16384"}
 export VIRT_CPUS=${VIRT_CPUS:-"8"}
-#export IMAGE_URL=${IMAGE_URL:-"https://cloud-images.ubuntu.com/trusty/current/trusty-server-cloudimg-amd64-disk1.img"}
-export IMAGE_URL=${IMAGE_URL:-"file:///home/wtw/libvirt/prod-xen.img"}
-export IMAGE_NAME=${IMAGE_NAME:-"prod-xen.img"}
+export IMAGE_URL=${IMAGE_URL:-"https://cloud-images.ubuntu.com/trusty/current/trusty-server-cloudimg-amd64-disk1.img"}
+#export IMAGE_URL=${IMAGE_URL:-"file:///home/wtw/libvirt/prod-xen.img"}
+#export IMAGE_NAME=${IMAGE_NAME:-"prod-xen.img"}
 export IPADDR_PREFIX=${IPADDR_PREFIX:-"10.200.200."}
 
